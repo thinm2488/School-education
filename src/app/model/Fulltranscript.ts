c59edddf63549c86fa5dfc56436ddf566ecd52be
thi.nm2488 <thi.nm2488@sinhvien.hoasen.edu.vn>
2019-10-26 16:37:01 +0700

    no message

--- src/app/model/Fulltranscript.ts
+++ src/app/model/Fulltranscript.ts
@@ -10,29 +10,253 @@ export default class FullTranscript {
     hanhKiem: '';
     nhanXet: '';
     HKI: {
-       
+        diemAnh:
+        {
             diemMieng: [],
             diem15p: [],
             diem1tiet: [],
-            diemGiuaKy: '',
-            diemCuoiKy: '',
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+ 
+           
 
-       
+        },
 
-        
+        diemToan: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            
+        },
+        diemVan: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+           
+        },
+        diemSinh: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            
+        },
+        diemSu: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            
+        },
+        diemDia: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+           
+        },
+        diemCongNghe: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            
+        },
+        diemLy: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+          
+        },
+        diemHoa: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+          
+        },
+        diemGDCD: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+           
+        },
+        diemTin: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+         
+        },
+        diemTheDuc: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+        },
+        diemMyThuat: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+        },
 
 
 
-    }
+    },
     HKII: {
-       
+        diemAnh:
+        {
             diemMieng: [],
             diem15p: [],
             diem1tiet: [],
-            diemGiuaKy: '',
-            diemCuoiKy: '',
-            diemTB: '',
-            TBCN:''
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+
+        diemToan: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemVan: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemSinh: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemSu: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemDia: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemCongNghe: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemLy: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemHoa: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemGDCD: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemTin: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemTheDuc: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+        diemMyThuat: {
+            diemMieng: [],
+            diem15p: [],
+            diem1tiet: [],
+            diemGiuaKy: "",
+            diemCuoiKy: "",
+            diemTB: "",
+            TBCN: ""
+        },
+
+
+
+
 
 
     }
