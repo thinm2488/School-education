6f1c8c624967270da136f39268e57a2bfb746773
thi.nm2488 <thi.nm2488@sinhvien.hoasen.edu.vn>
2019-10-26 16:24:32 +0700

    no message

--- api/model/Diligence.js
+++ api/model/Diligence.js
@@ -4,43 +4,8 @@ var DiligenceSchema = new Schema(
     {
      
         idHocSinh:{type:String},
-       
-        thang9:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang10:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang11:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang12:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang1:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang2:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang3:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang4:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
-        thang5:{
-            soLuong:{type:Number},
-            danhSachNgayNghi:[]
-        },
+        danhSachNgayNghi:[]
+        
     }
 );
 module.exports = mongoose.model('Diligence', DiligenceSchema)
